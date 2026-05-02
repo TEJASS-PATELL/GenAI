ce30fa5359bfc551ef3a5c5fd400d0320bc4ceb1
Tejas <tejasspatell2@gmail.com>
2026-05-02 23:40:23 +0530

    learning gemini with node

--- gemini.js
+++ gemini.js
@@ -2,22 +2,26 @@ import { GoogleGenAI } from "@google/genai";
 import { configDotenv } from "dotenv";
 configDotenv();
 
-const ai = new GoogleGenAI({apiKey: process.env.geminiKey});
+const ai = new GoogleGenAI({ apiKey: process.env.geminiKey });
 
 async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
-    contents: "Explain how AI works in a few words",
-    config:{
-        temperature: 0.5,
-        thinkingConfig: {
-            includeThoughts: true,
-            thinkingBudget:100
+    const response = await ai.models.generateContentStream({
+        model: "gemini-3-flash-preview",
+        contents: "Explain how AI works in a few words",
+        config:{
+            temperature: 0.5,
+            thinkingConfig: {
+                includeThoughts: true,
+                thinkingBudget:100
+            },
+            systemInstruction:"answer upto 500 words"
         },
-        systemInstruction:"answer upto 20 words"
-    },
-  });
-  console.log(response.text);
+    });
+
+    for await (const chunks of response) {
+        const text = chunks.text;
+        console.log(text);
+    }
 }
 
 main();
